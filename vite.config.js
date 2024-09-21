c0c5f2efdce5d730dc8c0e1d377f287a9e55dc0a
pragsYE <davinc.dn.1889@gmail.com>
2024-09-21 21:46:51 +0530

    init

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
+
+export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'server.crt')),
+    },
+    host: '192.168.241.194',  // Your network IP address
+    port: 3000
+  }
+});
